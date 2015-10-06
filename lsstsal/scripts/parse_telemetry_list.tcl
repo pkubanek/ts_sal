53014a6bf25dc67ec90d44e1b9001f374a809360
Dave Mills <dmills@lsst.org>
2015-10-06 14:26:01 -0700

    Modified  m2ms commands Updated html telemetry table generator

--- lsstsal/scripts/parse_telemetry_list.tcl
+++ lsstsal/scripts/parse_telemetry_list.tcl
@@ -8,17 +8,21 @@ source $SAL_DIR/add_private_idl.tcl
 set subsys [lindex $argv 0]
 exec mkdir -p $SAL_WORK_DIR/html/[set subsys]
 
-set all [lsort [glob [set subsys]*.idl]]
-set fout [open $SAL_WORK_DIR/html/[set subsys]/telemetry_[set subsys].html w]
+set all [lsort [glob idl-templates/validated/[set subsys]*.idl]]
+set fout [open $SAL_WORK_DIR/html/[set subsys]/[set subsys]_Telemetry.html w]
 puts stdout "Generating html Telemetry table $subsys"
-foreach i $all {
- set fin [open $i r]
- gets $fin rec
-puts $fout "<H3>[file rootname $i] Telemetry Stream</H3><P><UL>"
+foreach i [lsort $all] {
+ set ftyp [lindex [split [file tail $i] _] 1]
+ if { $ftyp != "command" && $ftyp != "logevent" } {
+  set fin [open $i r]
+  gets $fin rec
+puts $fout "<H3>[file tail [file rootname $i]] Telemetry Stream</H3><P><UL>"
 puts $fout "<TABLE BORDER=3 CELLPADDING=5 BGCOLOR=LightBlue WIDTH=700>
 <TR BGCOLOR=Yellow><B><TD>Stream name</TD><TD>Item</TD><TD>Type</TD><TD>Comment</TD></B></TR>"
- while { [gets $fin rec] > -1 } {
-   if { [llength $rec] > 1 } {
+  while { [gets $fin rec] > -1 } {
+   set rec [string trim $rec "\{\}"]
+   if { [lindex $rec 0] != "#pragma" } {
+    if { [llength $rec] > 1 } {
       set type [lindex $rec 0]
       set name [string trim [lindex $rec 1] "; "]
       set cmt "-"
@@ -26,12 +30,15 @@ puts $fout "<TABLE BORDER=3 CELLPADDING=5 BGCOLOR=LightBlue WIDTH=700>
          set cmt [lindex [split $rec "/"] 2]
       }
       if { [lindex [split $name "_"] 0] != "private" } {
-        puts $fout "<TR><B><TD>[file rootname $i]</TD><TD>$name</TD><TD>$type</TD><TD>$cmt</TD></B></TR>"
+        puts $fout "<TR><B><TD>[file tail [file rootname $i]]</TD><TD>$name</TD><TD>$type</TD><TD>$cmt</TD></B></TR>"
       }
+    }
    }
+  }
+  close $fin
+  puts $fout "</TABLE></UL><P></BODY></HTML>"
+  puts stdout "Processed $i"
  }
- close $fin
- puts $fout "</TABLE></UL><P></BODY></HTML>"
 }
  
 close $fout
