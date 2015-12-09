05d6cf307b4a2bc6d153c68b3d5b02f847309918
Dave Mills <dmills@lsst.org>
2015-12-09 10:45:49 -0700

    Updated dm telemetry Added copy to legacy-to-xml converter

--- lsstsal/scripts/convertIDLtlmToXML.tcl
+++ lsstsal/scripts/convertIDLtlmToXML.tcl
@@ -9,6 +9,9 @@ source $env(SAL_DIR)/SALTopicTemplateXML.tcl
 source $env(SAL_DIR)/versioning.tcl
 
 set idl [glob [set argv]*.idl]
-checkidl $idl
+exec cat $idl > idl-templates/[set argv]_Telemetry.idl
+checkidl [set argv]_Telemetry.idl
 writeXMLsubsys ./xml $argv
+$env(SAL_DIR)/parse_telemetry_list.tcl $argv
+exec cp html/[set argv]/[set argv]_Telemetry.html $env(SAL_DIR)/icd/LSE74-html/appendixb/[set argv]/.
 
