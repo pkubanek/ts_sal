e80fff090c3fd26fdc0826875eb95bb0647e2e91
Dave Mills <dmills@lsst.org>
2016-05-03 12:59:17 -0700

    Change internal SALVERSION identifier

--- lsstsal/scripts/sal_version.tcl
+++ lsstsal/scripts/sal_version.tcl
@@ -1,4 +1,4 @@
-set SALVERSION 2.5.3
+set SALVERSION 3.0
 set SAL_BASE_DIR $env(SAL_DIR)/scripts
 set SAL_CMAKE_DIR $SAL_BASE_DIR/code/simd/cmake
 set SAL_SID(auxscope) 		1000
@@ -11,7 +11,7 @@ set SAL_SID(hexapod) 		7000
 set SAL_SID(lasercal) 		8000
 set SAL_SID(m1m3) 		9000
 set SAL_SID(m2ms) 		10000
-set SAL_SID(mount) 		11000
+set SAL_SID(MTMount) 		11000
 set SAL_SID(network) 		12000
 set SAL_SID(ocs) 		13000
 set SAL_SID(power) 		14000
