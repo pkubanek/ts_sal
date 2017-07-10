4c1d8ff59a72e9759dad0a8a4e2a75383345fe3b
Dave Mills <DMills@lsst.org>
2017-07-10 13:16:59 -0700

    Fixed to allow any install location

--- lsstsal/scripts/ospl_version.tcl
+++ lsstsal/scripts/ospl_version.tcl
@@ -1,6 +1,6 @@
 set OSPL_HDE $env(OSPL_HOME)
 set ospl [lsearch [split $OSPL_HDE) "/"] OpenSpliceDDS]
-set OSPL_VERSION [string trim [lindex [split $OSPL_HDE) "/"]  5] V]
+set OSPL_VERSION [string trim [lindex [split $OSPL_HDE) "/"]  [expr $ospl +1] ] V]
 set l  [llength [split $OSPL_VERSION .]]
 if { $l == 2 } {set OSPL_VERSION $OSPL_VERSION.0}
 set SIMD_BASE_DIR /opt/simd
