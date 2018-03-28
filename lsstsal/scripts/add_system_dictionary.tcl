94eea9256ec13858d55fd9281bdca245d34579bc
Dave Mills <DMills@lsst.org>
2018-03-28 09:33:14 -0700

    Spelling correction atHeaderService

--- lsstsal/scripts/add_system_dictionary.tcl
+++ lsstsal/scripts/add_system_dictionary.tcl
@@ -24,17 +24,17 @@ set SYSDIC(catchuparchiver,hasGenerics) 1
 set SYSDIC(eec,hasGenerics) 1
 set SYSDIC(efd,hasGenerics) 1
 set SYSDIC(headerService,hasGenerics) 1
-set SYSDIC(hexapod,hasGenerics) 1
+####set SYSDIC(hexapod,hasGenerics) 1
 set SYSDIC(m1m3,hasGenerics) 1
 set SYSDIC(processingcluster,hasGenerics) 1
-set SYSDIC(rotator,hasGenerics) 1
+####set SYSDIC(rotator,hasGenerics) 1
 set SYSDIC(summitFacility,hasGenerics) 1
 set SYSDIC(tcsAOCS,hasGenerics) 1
 set SYSDIC(tcsOfc,hasGenerics) 1
 set SYSDIC(tcsWEP,hasGenerics) 1
 set SYSDIC(vms,hasGenerics) 1
 
-set SYSDIC(systems) "atcamera atheaderService accl archiver atcs atMonochromator atScheduler atWhiteLight auxscope calibrationCBP calibrationMonochromator calibrationSpectrometer calibrationElectrometer camera 
+set SYSDIC(systems) "atcamera atHeaderService accl archiver atcs atMonochromator atScheduler atWhiteLight auxscope calibrationCBP calibrationMonochromator calibrationSpectrometer calibrationElectrometer camera 
 catchuparchiver comcam headerService dome domeADB domeAPS domeLouvers domeLWS domeMONCS domeTHCS eec efd environment EXA hexapod lasercal m1m3 m2ms MTMount network ocs operations power processingcluster rotator sequencer scheduler seeing skycam summitFacility system tcs tcsAOCS tcsOfc tcsWEP vms"
 
 foreach s $SYSDIC(systems) {set SYSDIC($s,type) system}
