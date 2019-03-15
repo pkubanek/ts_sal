5229bc65a85d203bfefef897c993bea68314701c
Russell Owen <rowen@uw.edu>
2019-03-15 15:41:17 -0700

    Keep most files generated by make_salpy_lib
    Modify make_salpy_lib to start by deleting generated files
    and end by leaving most of them. The code still deletes the
    "html", "include" and "lib" directories because they don't
    seem to contain anything useful.

--- python/lsst/ts/sal/make_salpy_lib.py
+++ python/lsst/ts/sal/make_salpy_lib.py
@@ -47,68 +47,71 @@ def make_salpy_lib(sal_name, demo=False):
     demo : `bool`
         If True build a demo reader and writer for each topic.
     """
-    print(f"*** Make SALPY_{sal_name} library ***")
+    print(f"* Make SALPY_{sal_name} libraries ***")
     sal_dir = get_env_dir("TS_SAL_DIR", "ts_sal not setup")
     xml_dir = get_env_dir("TS_XML_DIR", "ts_xml not setup")
     sal_work_dir = get_env_dir("SAL_WORK_DIR", "$SAL_WORK_DIR must be defined")
     ld_lib_path_name = "LD_LIBRARY_PATH"
     initial_ld_lib_path = os.environ[ld_lib_path_name]
 
-    try:
-        os.environ[ld_lib_path_name] = f"{sal_work_dir}/lib:{initial_ld_lib_path}"
-        lib_names = dict(
-            cpp=f"libsacpp_{sal_name}_types.so",
-            python=f"SALPY_{sal_name}.so")
-        src_lib_paths = dict(
-            cpp=os.path.join(sal_work_dir, sal_name, "cpp", lib_names["cpp"]),
-            python=os.path.join(sal_work_dir, sal_name, "cpp", "src", lib_names["python"]))
-        dest_lib_paths = dict(
-            cpp=os.path.join(sal_dir, "lib", lib_names["cpp"]),
-            python=os.path.join(sal_dir, "python", lib_names["python"]))
+    print(f"*** Cleanup old {sal_name} files ***")
+    os.environ[ld_lib_path_name] = initial_ld_lib_path
+    xmlfiles = glob.glob(os.path.join(sal_work_dir, "*.xml"))
+    for f in xmlfiles:
+        os.remove(f)
 
-        for path in src_lib_paths.values():
-            if os.path.isfile(path):
-                print(f"Remove {path}")
-                os.remove(path)
+    sal_name_dirs = glob.glob(os.path.join(sal_work_dir, f"{sal_name}*"))
+    for name_dir in sal_name_dirs:
+        shutil.rmtree(os.path.join(name_dir), ignore_errors=True)
 
-        print(f"*** Copy {sal_name} XML files ***")
-        interfaces_dir = os.path.join(xml_dir, "sal_interfaces")
-        std_xml_paths = [os.path.join(interfaces_dir, f"{n}.xml") for n in ("SALGenerics", "SALSubsystems")]
-        sal_xml_paths = glob.glob(os.path.join(interfaces_dir, sal_name, "*.xml"))
-        if not sal_xml_paths:
-            raise RuntimeError(f"Could not find any XML files for SAL component {sal_name}")
-        for xmlpath in std_xml_paths + sal_xml_paths:
-            shutil.copy(xmlpath, sal_work_dir)
+    for subdir in ("html", "include", "lib"):
+        shutil.rmtree(os.path.join(sal_work_dir, subdir), ignore_errors=True)
 
-        print(f"*** Validate and generate {sal_name} libraries ***")
-        demo_suffix = "" if demo else " fastest"
-        for command in ("validate", "html", f"sal cpp{demo_suffix}", f"sal python{demo_suffix}"):
-            full_cmd = f"salgenerator {sal_name} {command}"
-            print(f"***** {full_cmd}")
-            subprocess.run(full_cmd, check=True, cwd=sal_work_dir, shell=True)
+    os.environ[ld_lib_path_name] = f"{sal_work_dir}/lib:{initial_ld_lib_path}"
+    lib_names = dict(
+        cpp=f"libsacpp_{sal_name}_types.so",
+        python=f"SALPY_{sal_name}.so")
+    src_lib_paths = dict(
+        cpp=os.path.join(sal_work_dir, sal_name, "cpp", lib_names["cpp"]),
+        python=os.path.join(sal_work_dir, sal_name, "cpp", "src", lib_names["python"]))
+    dest_lib_paths = dict(
+        cpp=os.path.join(sal_dir, "lib", lib_names["cpp"]),
+        python=os.path.join(sal_dir, "python", lib_names["python"]))
 
-        print(f"*** Move {sal_name} libraries into place ***")
-        for path in src_lib_paths.values():
-            if not os.path.isfile(path):
-                raise RuntimeError(f"{path} not generated")
-        for name in src_lib_paths.keys():
-            src_path = src_lib_paths[name]
-            dest_path = dest_lib_paths[name]
-            if os.path.exists(dest_path):
-                os.remove(dest_path)
-            os.rename(src_path, dest_path)
+    for path in src_lib_paths.values():
+        if os.path.isfile(path):
+            print(f"Remove {path}")
+            os.remove(path)
 
-    finally:
-        print(f"*** Cleanup {sal_name} files ***")
-        os.environ[ld_lib_path_name] = initial_ld_lib_path
-        xmlfiles = glob.glob(os.path.join(sal_work_dir, "*.xml"))
-        for f in xmlfiles:
-            os.remove(f)
+    print(f"*** Copy {sal_name} XML files ***")
+    interfaces_dir = os.path.join(xml_dir, "sal_interfaces")
+    std_xml_paths = [os.path.join(interfaces_dir, f"{n}.xml") for n in ("SALGenerics", "SALSubsystems")]
+    sal_xml_paths = glob.glob(os.path.join(interfaces_dir, sal_name, "*.xml"))
+    if not sal_xml_paths:
+        raise RuntimeError(f"Could not find any XML files for SAL component {sal_name}")
+    for xmlpath in std_xml_paths + sal_xml_paths:
+        shutil.copy(xmlpath, sal_work_dir)
 
-        sal_name_dirs = glob.glob(os.path.join(sal_work_dir, f"{sal_name}*"))
-        for name_dir in sal_name_dirs:
-            shutil.rmtree(os.path.join(name_dir), ignore_errors=True)
+    print(f"*** Validate and generate {sal_name} libraries ***")
+    demo_suffix = "" if demo else " fastest"
+    for command in ("validate", "html", f"sal cpp{demo_suffix}", f"sal python{demo_suffix}"):
+        full_cmd = f"salgenerator {sal_name} {command}"
+        print(f"***** {full_cmd}")
+        subprocess.run(full_cmd, check=True, cwd=sal_work_dir, shell=True)
 
-        for subdir in ("html", "idl-templates", "include", "lib", "sql"):
-            shutil.rmtree(os.path.join(sal_work_dir, subdir), ignore_errors=True)
-        print(f"*** Done generating SALPY_{sal_name} ***")
+    print(f"*** Move {sal_name} libraries into place ***")
+    for path in src_lib_paths.values():
+        if not os.path.isfile(path):
+            raise RuntimeError(f"{path} not generated")
+    for name in src_lib_paths.keys():
+        src_path = src_lib_paths[name]
+        dest_path = dest_lib_paths[name]
+        if os.path.exists(dest_path):
+            os.remove(dest_path)
+        os.rename(src_path, dest_path)
+
+    print(f"*** Final cleanup ***")
+    for subdir in ("html", "include", "lib"):
+        shutil.rmtree(os.path.join(sal_work_dir, subdir), ignore_errors=True)
+
+    print(f"*** Done generating SALPY_{sal_name} ***")
