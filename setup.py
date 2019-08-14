a84767c4966df68e056988ced640c8630f73c253
couger01 <couger01@luther.edu>
2019-08-14 15:44:14 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_namespace_packages
+
+install_requires = []
+tests_require = []
+dev_requires = install_requires + tests_require + ["documenteer[pipelines]"]
+
+setup(
+    name="ts_sal",
+    description="Only sets up the python code written for SAL.",
+    use_scm_version=True,
+    setup_requires=["setuptools_scm"],
+    package_dir={"": "python"},
+    packages=find_namespace_packages(where="python"),
+    scripts=["bin.src/make_idl_files.py", "bin.src/make_salpy_libs.py"],
+    # tests_require=tests_require,
+    extras_require={"dev": dev_requires},
+    license="GPL",
+    project_url={
+        "Bug Tracker": "https://jira.lsstcorp.org/secure/Dashboard.jspa",
+        "Source Code": "https://github.com/lsst-ts/ts_idl",
+    }
+)
