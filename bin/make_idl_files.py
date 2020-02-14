edcd9f8b5089d086f6be9b1eb92976044c9fc069
Russell Owen <rowen@uw.edu>
2020-02-14 11:26:08 -0800

    Fix the ability to build all files in bin/make_idl_files.py
    Get rid of all_component_names from make_idl_file.py since it was broken and we no longer need it.

--- bin/make_idl_files.py
+++ bin/make_idl_files.py
@@ -1,25 +1,32 @@
 #!/usr/bin/env python
 import argparse
 
-from lsst.ts.sal.make_idl_file import all_component_names, make_idl_file
+import lsst.ts.xml
+from lsst.ts.sal.make_idl_file import make_idl_file
 
 parser = argparse.ArgumentParser(
     description="Build the IDL file for one or more SAL component")
-parser.add_argument("components", nargs='+',
-                    help="Names of SAL components, e.g. 'Test'; "
-                    "if 'all' then make IDL files for all SAL components")
+parser.add_argument("components", nargs='*',
+                    help="Names of SAL components, e.g. 'Script ScriptQueue'. "
+                    "Ignored if --all is specified")
+parser.add_argument("--all", action="store_true",
+                    help="Make all components except those listed in --exclude.")
+parser.add_argument("--dry-run", action="store_true",
+                    help="Print the component names that would be built and exit.")
 parser.add_argument("--exclude", nargs="+",
-                    help="Names of SAL copmonents to exclude")
+                    help="Names of SAL components to exclude. "
+                    "If an entry appears in both `components` and `exclude` then it is excluded.")
 
 args = parser.parse_args()
 
-if args.components == ["all"]:
-    components = all_component_names()
+if args.all:
+    components = lsst.ts.xml.subsystems
 else:
     components = args.components
 if args.exclude:
     exclude = set(args.exclude)
     components = [name for name in components if name not in exclude]
 print(f"Making IDL files for: {', '.join(components)}")
-for name in components:
-    make_idl_file(name=name)
+if not args.dry_run:
+    for name in components:
+        make_idl_file(name=name)
